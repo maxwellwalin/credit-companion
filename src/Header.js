411464f5fbc15680ec34cdb7d8d21e1b30719b4c
maxwell walin <maxwellwalin@gmail.com>
2022-02-05 16:59:23 -0800

    fixed copyright link, adjusted header code structure, changed viewport settings

--- src/Header.js
+++ src/Header.js
@@ -3,12 +3,28 @@ import Link from "./Link";
 
 export default function Header() {
     return (
-        <Box component="header" display={'flex'} justifyContent='center' boxShadow="1px 0.3px 3px">
-            <Box py={3} display="flex" justifyContent="space-between" width={'1536px'}>
-                <Typography variant='h4' component={Link} href="/" sx={{ textDecoration: 'none', fontWeight: 'bold' }}>
-                    Credit Companion
+        <Box
+            component="header"
+            display={'flex'}
+            justifyContent='center'
+            boxShadow="1px 0.3px 3px"
+            px={2}>
+            <Box
+                py={3}
+                display="flex"
+                justifyContent="space-between"
+                width={'1536px'}>
+                <Typography component={Link} href="/" sx={{ textDecoration: 'none' }}>
+                    <Typography variant='h4' component='h1' sx={{ fontWeight: 'bold' }}>
+                        Credit Companion
+                    </Typography>
                 </Typography>
-                <Box component="nav" display={'grid'} gridTemplateColumns={'auto auto auto'} justifyItems={'center'} gap={2}>
+                <Box
+                    component="nav"
+                    display={'grid'}
+                    gridTemplateColumns={'auto auto auto'}
+                    justifyItems={'center'}
+                    gap={2}>
                     <Button variant="contained" component={Link} noLinkStyle href="/quiz">
                         Take the credit quiz
                     </Button>
