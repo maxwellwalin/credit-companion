043812440713a98abf9c711c9b3478661b0948e2
Revivedaniel <mlg.danielstark@gmail.com>
2022-02-07 21:23:34 -0800

    Added placeholder images

--- pages/login.js
+++ pages/login.js
@@ -22,49 +22,22 @@ export default function Login() {
                 <div className="sign-in-detail text-white">
                   <a className="sign-in-logo mb-5" href="#">
                     <img
-                      src="../assets/images/logo-full.png"
+                      src="https://via.placeholder.com/215x50?text=Logo"
                       className="img-fluid"
                       alt="logo"
                     />
                   </a>
                   <div className="sign-slider overflow-hidden ">
-                    <ul className="swiper-wrapper list-inline m-0 p-0 ">
-                      <li className="swiper-slide">
+                    <ul className="list-inline m-0 p-0 ">
+                      <li>
                         <img
-                          src="../assets/images/login/1.png"
+                          src="https://via.placeholder.com/292x157?text=Random+Image"
                           className="img-fluid mb-4"
                           alt="logo"
                         />
-                        <h4 className="mb-1 text-white">Find new friends</h4>
+                        <h4 className="mb-1 text-white">Fix your credit</h4>
                         <p>
-                          It is a long established fact that a reader will be
-                          distracted by the readable content.
-                        </p>
-                      </li>
-                      <li className="swiper-slide">
-                        <img
-                          src="../assets/images/login/2.png"
-                          className="img-fluid mb-4"
-                          alt="logo"
-                        />
-                        <h4 className="mb-1 text-white">
-                          Connect with the world
-                        </h4>
-                        <p>
-                          It is a long established fact that a reader will be
-                          distracted by the readable content.
-                        </p>
-                      </li>
-                      <li className="swiper-slide">
-                        <img
-                          src="../assets/images/login/3.png"
-                          className="img-fluid mb-4"
-                          alt="logo"
-                        />
-                        <h4 className="mb-1 text-white">Create new events</h4>
-                        <p>
-                          It is a long established fact that a reader will be
-                          distracted by the readable content.
+                          lorem ipsum dolor sit amet, consectetur adipiscing
                         </p>
                       </li>
                     </ul>
@@ -126,6 +99,7 @@ export default function Login() {
                           type="checkbox"
                           className="form-check-input"
                           id="customCheck1"
+                          style={{cursor: 'pointer'}}
                         />
                         <label
                           className="form-check-label"
@@ -169,7 +143,6 @@ export default function Login() {
             </div>
           </div>
         </section>
-        <script src="../assets/js/slider.js"></script>
       </SignInPage>
       <Footer />
     </>
